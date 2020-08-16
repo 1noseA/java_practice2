1a30fbe287e9869297302e6ad03c46d5df92dcb0
1noseA <1noseA2L@gmail.com>
2020-08-16 13:10:35 +0900

    [6-1-4]関数オブジェクト

--- Main.java
+++ Main.java
@@ -1,7 +1,15 @@
-import java.util.*;
+import java.util.function.*;
 
 public class Main {
+  public static int sub(int a, int b) {
+    return a - b;
+  }
+
   public static void main(String[] args) {
-    
+    // subメソッドの処理ロジックを変数funcに代入する
+    IntBinaryOperator func = Main::sub;
+    // 変数funcに格納されている処理ロジックを引数５と３で実行する
+    int a = func.applyAsInt(5, 3);
+    System.out.println("5-3は" + a);
   }
 }
