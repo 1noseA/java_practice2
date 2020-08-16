b8d495afdd0053258196581d09f7326ab2babf2f
1noseA <1noseA2L@gmail.com>
2020-08-16 13:49:49 +0900

    [6-2]ラムダ式

--- Main.java
+++ Main.java
@@ -1,15 +1,9 @@
 import java.util.function.*;
 
 public class Main {
-  public static int sub(int a, int b) {
-    return a - b;
-  }
-
   public static void main(String[] args) {
-    // subメソッドの処理ロジックを変数funcに代入する
-    MyFunction func = Main::sub;
-    // 変数funcに格納されている処理ロジックを引数５と３で実行する
-    int a = func.call(5, 3);
+    IntBinaryOperator func = (int a, int b) -> {return a - b;};
+    int a = func.applyAsInt(5, 3);
     System.out.println("5-3は" + a);
   }
 }
