3f363ff844044b9894c64de2ab16a7210f69dd3d
1noseA <1noseA2L@gmail.com>
2020-08-16 13:21:31 +0900

    [6-1-6]SAMインターフェース

--- Main.java
+++ Main.java
@@ -7,9 +7,9 @@ public class Main {
 
   public static void main(String[] args) {
     // subメソッドの処理ロジックを変数funcに代入する
-    IntBinaryOperator func = Main::sub;
+    MyFunction func = Main::sub;
     // 変数funcに格納されている処理ロジックを引数５と３で実行する
-    int a = func.applyAsInt(5, 3);
+    int a = func.call(5, 3);
     System.out.println("5-3は" + a);
   }
 }
---
+++ MyFunction.java
@@ -0,0 +1,5 @@
+// MyFunction型を使えるようにSAMインターフェースを定義する
+// インターフェース名やメソッド名は自由に決めていい
+public interface MyFunction {
+  public abstract int call(int x, int y) ;
+}
