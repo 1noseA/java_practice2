eaa211c26a3c192cddb8573ef24e718792b5bb18
1noseA <1noseA2L@gmail.com>
2020-08-17 15:21:35 +0900

    [8-4]ログ出力ライブラリ（エラー）

--- Bank.java
+++ Bank.java
@@ -1,16 +1,16 @@
-import org.apache.commons.lang3.builder.*;
+// import org.apache.commons.lang3.builder.*;
 
-public class Bank {
-  String name;
-  String address;
+// public class Bank {
+//   String name;
+//   String address;
 
-  // この一行だけで「全てのフィールドが等価ならインスタンスも等価とみなす」
-  public boolean equals(Object o) {
-    return EqualsBuilder.reflectionEquals(this, o);
-  }
+//   // この一行だけで「全てのフィールドが等価ならインスタンスも等価とみなす」
+//   public boolean equals(Object o) {
+//     return EqualsBuilder.reflectionEquals(this, o);
+//   }
 
-  // この一行だけでハッシュ値を生成できる
-  public int hashCode() {
-    return HashCodeBuilder.reflectionHashCode(this);
-  }
-}
+//   // この一行だけでハッシュ値を生成できる
+//   public int hashCode() {
+//     return HashCodeBuilder.reflectionHashCode(this);
+//   }
+// }
