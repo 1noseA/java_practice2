a69d37c8d387fa37aaa08c354ae758db25d13ada
1noseA <1noseA2L@gmail.com>
2020-08-17 11:25:57 +0900

    [7-4-2]システムプロパティ

--- Main.java
+++ Main.java
@@ -1,9 +1,16 @@
-import java.util.function.*;
+import java.util.*;
 
 public class Main {
   public static void main(String[] args) {
-    IntBinaryOperator func = (int a, int b) -> {return a - b;};
-    int a = func.applyAsInt(5, 3);
-    System.out.println("5-3は" + a);
+    System.out.println("ご利用のJavaバージョン：");
+    System.out.println(System.getProperty("Java.version"));
+    Properties p = System.getProperties();
+    Iterator<String> i = p.stringPropertyNames().iterator();
+    System.out.println("【システムプロパティー覧】");
+    while(i.hasNext()) {
+      String key = i.next();
+      System.out.print(key + "=");
+      System.out.println(System.getProperty(key));
+    }
   }
 }
