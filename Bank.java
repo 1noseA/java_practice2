176b12868f5bd61f15d8d6dbcd05673e3f3bcb15
1noseA <1noseA2L@gmail.com>
2020-08-17 15:01:00 +0900

    [8-3]commons-lang（エラー）

---
+++ Bank.java
@@ -0,0 +1,16 @@
+import org.apache.commons.lang3.builder.*;
+
+public class Bank {
+  String name;
+  String address;
+
+  // この一行だけで「全てのフィールドが等価ならインスタンスも等価とみなす」
+  public boolean equals(Object o) {
+    return EqualsBuilder.reflectionEquals(this, o);
+  }
+
+  // この一行だけでハッシュ値を生成できる
+  public int hashCode() {
+    return HashCodeBuilder.reflectionHashCode(this);
+  }
+}
